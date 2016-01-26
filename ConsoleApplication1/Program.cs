37495bc489c635f72206f01e0062b8a1859444a0
zacglenn <pluszac@gmail.com>
2016-01-26 09:36:41 -0600

    added code to ensure we grab all the users by increasing the recordsPerPage value

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -24,19 +24,23 @@ namespace ConsoleApplication1
             myIE.TextField(Find.ByName("password")).TypeText(password);
             myIE.Button(Find.ByName("SUBMIT1")).Click();
 
-            //nav to url
+            //nav to url containing userList
             IE myIE3 = new IE(shoreTelUserList);
-            var temp = myIE3.Table(Find.By("_t", "Users"));
+
+            //change recPerPage to 3000 to ensure we grab all extensions
+            myIE3.SelectList(Find.ById("RecPerPage")).Option("3000").Select();
+
+            var userListTable = myIE3.Table(Find.By("_t", "Users"));
 
             //Prepare the ports!
             Console.WriteLine("var sports = new ActiveXObject('ShoreBusDS.sPorts');");
 
-            foreach (var tr in temp.TableRows)
+            foreach (var tableRow in userListTable.TableRows)
             {
                 //sample text layout of each tr.Text: " Zac Glenn Headquarters Executives Personal 7777 7777 AB77 77 Zac Glenn Home "
-                if (!string.IsNullOrEmpty(tr.Text))
+                if (!string.IsNullOrEmpty(tableRow.Text))
                 {
-                    Match match = Regex.Match(tr.Text, @"\d{4}");
+                    Match match = Regex.Match(tableRow.Text, @"\d{4}");
                     string extension = match.Value;
                     Console.WriteLine("sports.UserGoHome('" + extension + "');");
                 }
